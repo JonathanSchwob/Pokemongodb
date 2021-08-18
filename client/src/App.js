9ec4125fcd220b9d023a015544e1f267787ae0a8
JonathanSchwob <jonathanrschwob@gmail.com>
2021-08-18 02:40:41 -0500

    render names to client, but out of order

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch('api/pokemon')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.map(item => (
+          // console.log(item.name, 'item')
+          <div key={item.id}>
+            {item.name.english}
+          </div>
+        ))}
+        {/* {console.log(items[1], 'items')} */}
+      </ul>
+    );
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+var db = require('./server/db/index.js');
+var modelModule = require('./server/pokemon/Pokemon.js');
+var model = modelModule.Pokemon;
 
 app.get('/', (req, res) => {
   res.send('Helo World!')
 })
+
+app.get('/api/pokemon', function(req, res) {
+  model.find(function(err, doc) {
+    if (err) {
+      console.log('error on get:', err);
+    } else {
+      res.send(doc);
+    }
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
